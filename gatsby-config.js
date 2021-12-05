ae60c9adf34e924607947ce9e5436fbf4d7c90b4
Miguel <chanampamiguel@gmail.com>
2021-12-05 11:59:09 -0300

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -31,6 +32,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: '4579472918a71bc170af217043d424'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/hooks/use-seo.js
@@ -0,0 +1,22 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+const useSeo = () => {
+    
+    const data = useStaticQuery(graphql`
+        query {
+            datoCmsSite {
+                globalSeo {
+                    titleSuffix
+                    fallbackSeo {
+                        title
+                        description
+                    }
+                }
+            }
+        }
+    `)
+    
+    return data.datoCmsSite.globalSeo;
+}
+ 
+export default useSeo;
